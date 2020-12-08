6872e3058e215222816d688996123e3ddbd461b9
Jason Copas <jason.k.copas@gmail.com>
2020-12-08 14:30:48 -0800

    Junos Stuff is borked.

--- bulkEdit.py
+++ bulkEdit.py
@@ -3,17 +3,33 @@ from netmiko import ConnectHandler
 
 user = input("username: ")
 password = getpass.getpass()
+
 with open('ciscoSwitches') as ciscoIPs:
-    lines = ciscoIPs.read().splitlines()
-for line in lines:
-    device = {
+    ciscoDevs = ciscoIPs.read().splitlines()
+for ciscoIP in ciscoDevs:
+    ciscoDev = {
         'device_type': 'cisco_ios',
-        'ip': line,
+        'ip': ciscoIP,
         'username': user,
         'password': password
     }
-    with open('iosv_l2_cisco_design') as config:
-        edit = config.read().splitlines()
-        net_connect = ConnectHandler(**device)
-        output = net_connect.send_config_set(edit)
+    with open('ios_python.config') as ciscoConfig:
+        ciscoEdit = ciscoConfig.read().splitlines()
+        net_connect = ConnectHandler(**ciscoDev)
+        output = net_connect.send_config_set(ciscoEdit)
         print(output)
+
+#with open('junosDevices') as junosIPs:
+#    junosDevs = junosIPs.read().splitlines()
+#for junosIP in junosDevs:
+#    junosDev = {
+#        'device_type': 'juniper',
+#        'ip': junosIP,
+#        'username': user,
+#        'password': password
+#    }
+#    with open('vsrx_python.config') as juniperConfig:
+#        juniperEdit = juniperConfig.read().splitlines()
+#        net_connect = ConnectHandler(**junosDev)
+#        output = net_connect.send_config_set(juniperEdit)
+#        print(output)
