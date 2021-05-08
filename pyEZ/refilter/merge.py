ede87eb6c274e5cce93eaa4221d73f79c257b563
Jason Copas <jason.k.copas@gmail.com>
2021-05-08 08:41:56 -0700

    added pyEZ

---
+++ pyEZ/refilter/merge.py
@@ -0,0 +1,53 @@
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+import jxmlease
+import getpass
+import yaml
+import jinja2
+
+# Interactive Prompts
+device_name = input('Device to connect to: ')
+user_name = input('SSH Username: ')
+password = getpass.getpass()
+
+# Builds the objects that we will be calling throughout the script
+dev = Device(device_name, user=user_name, passwd=password)
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+
+# Retrieve BGP information from the device
+dev.open()
+xml_filter = '<protocols><bgp><group><neighbor/></group></bgp></protocols>'
+bgp_config = dev.rpc.get_config(filter_xml=xml_filter, options={'format':'json'})
+dev.close()
+
+# Identifiy what neighbors are configured for the device
+bgp_neighbors = bgp_config['configuration']['protocols']['bgp']['group'][0]['neighbor']
+neighbor_list = []
+for line in bgp_neighbors:
+    neighbor_list.append(line['name'])
+neighbor_dict = {}
+neighbor_dict['neighbors'] = neighbor_list
+
+# Start building the yaml file to fill out the refilter jinja2 template
+vars_template = templateEnv.get_template('varsTemplate.yml')
+vars_yaml = vars_template.render(neighbor_dict)
+with open(device_name + '.yml', 'w') as f:
+    print(vars_yaml, file=f)
+
+# Start building the configuration for the device
+config_vars = yaml.load(open(device_name + '.yml').read(), Loader=yaml.FullLoader)
+conf_template = templateEnv.get_template('refilter.conf')
+config = conf_template.render(config_vars)
+config_path = device_name + '.conf'
+with open(config_path, 'w') as f:
+    print(config, file=f)
+
+
+with Config(dev.open(), mode='exclusive') as cu:
+    cu.load(path=config_path, format='text', merge=True)
+    cu.pdiff()
+    cu.rollback()
+
+
+dev.close()
